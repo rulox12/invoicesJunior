df1d2419f6ad5038e080e2b20dfa4c7734f753a1
daniel betancur <daniel.betancur@placetopay.com>
2019-12-07 14:09:47 -0500

    Add view required for user control

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card text-center">
+        <div class="card-header">
+            <div class="d-flex justify-content-between">
+                <div class="p-2 h4">{{ __('Create Customer')  }}</div>
+
+                <div class="p-2">
+                    <a href="{{route('users.index')}}" class="btn btn btn-secondary" role="button"
+                       aria-disabled="true">
+                        {{__('Back')}}
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <br>
+            <div class="container-fluid">
+                <form action="{{ route('users.store') }}" method=post
+                      oninput='password2.setCustomValidity
+                          (password2.value != password.value ? "{{__('Passwords do not match')}}" : "")'>
+                    {{ csrf_field() }}
+                    @include('users.__form')
+                    <div class="card-footer d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary">{{ __('Create User') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
