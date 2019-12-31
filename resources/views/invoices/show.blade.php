b3edac60b2518fa393fb70e45bf18ca3a15e9ebe
Macbook air <danielpcpx@hotmail.com>
2019-12-31 17:08:19 -0500

    Customer detail is added to the invoice detail

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -47,6 +47,33 @@
                 </dl>
             </div>
         </div>
+    </div>
+    <!--Customer-->
+    <div class="card card-default">
+        <div class="card-header">
+            <div class="d-flex justify-content-between">
+                <div class="mr-auto p-2 h4">{{ __('Customer Details') }}</div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <div class="container-fluid">
+                <br>
+                <dl class="row text-center">
+                    <dt class="col-md-2 text-left">{{ __('Name') }}:</dt>
+                    <dd class="col-md-4 text-left">{{ $invoice->customer->name ." ". $invoice->customer->surname }}</dd>
+                    <dt class="col-md-2 text-left">{{ __('Identification') }}:</dt>
+                    <dd class="col-md-4 text-left">{{ $invoice->customer->type_document . " " .$invoice->customer->document }}</dd>
+                    @if($invoice->customer->state )
+                        <dt class="col-md-2 text-left">{{ __('State') }}:</dt>
+                        <dd class="col-md-2 text-left">{{ __('Active') }}</dd>
+                    @else
+                        <dt class="col-md-2 text-left">{{ __('State') }}:</dt>
+                        <dd class="col-md-2 text-left">{{ __('Inactive') }}</dd>
+                    @endif
+                </dl>
+            </div>
+        </div>
 
 
     </div>
