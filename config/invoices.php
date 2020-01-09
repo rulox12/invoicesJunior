b1b4e9e9ce115fab69ca3209827d961984a8f71f
Macbook air <danielpcpx@hotmail.com>
2020-01-08 23:18:40 -0500

    The use of laravel mix

---
+++ config/invoices.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'state' => [
+        0 => "Approved",
+        1 => "Rejected",
+        2 => "Failed",
+        3 => "Pending"
+    ],
+    'type' => [
+        0 => "Factura de Venta	",
+    ],
+];
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@
  */
 
 require('./bootstrap');
-
 window.Vue = require('vue');
 
 /**
@@ -30,3 +29,59 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+
+    feather.replace()
+
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [{
+                data: [
+                    15339,
+                    21345,
+                    18483,
+                    24003,
+                    23489,
+                    24092,
+                    12034
+                ],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    })
+}())
+
+
