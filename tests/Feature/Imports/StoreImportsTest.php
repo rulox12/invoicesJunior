3b8c0372d63fc30206357dce04e8d28728448f5d
daniel fernando betancur villada <danielpcpx@hotmail.com>
2020-01-14 21:12:23 -0500

    The tests are created to save and view the main import page

---
+++ tests/Feature/Imports/StoreImportsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Tests\Feature\Imports;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class StoreImportsTest  extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test * */
+    public function an_authenticated_user_can_upload_xlsx_files()
+    {
+        Excel::fake();
+
+        $this->actingAs($this->defaultUser())
+            ->post(route('imports.store'), [
+                'file' => UploadedFile::fake()->create('ejemplo.xlsx'),
+                'type' => 'App\Imports\InvoicesImport'
+            ])
+            ->assertSessionHasNoErrors()
+            ->assertRedirect();
+    }
+
+}
