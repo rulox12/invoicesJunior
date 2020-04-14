d22ab3eb03116d56745031d7529e107ff84f1593
Daniel Betancur <daniel.betancur@placetopay.com>
2020-04-13 20:59:31 -0500

    The user entity is suitable for the api

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -5,13 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
     use HasRoles;
-
+    use HasApiTokens;
 
     protected $primaryKey = 'id';
     /**
