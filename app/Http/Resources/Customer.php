0a5df908955f89858676e2e7fad19b1c86009ae4
Daniel Betancur <daniel.betancur@placetopay.com>
2020-04-13 21:00:11 -0500

    Create Api controller for Customer

---
+++ app/Http/Resources/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Customer extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'type_document' => $this->type_document,
+            'document' => $this->document,
+            'state' => $this->state,
+        ];
+    }
+}
